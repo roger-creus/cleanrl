8cecb52fe2ab53d2c779f61669ce2383a44a7e70
roger-creus <creus99@gmail.com>
2024-08-05 19:09:56 -0400

    wip

--- cleanrl/pqn_atari_envpool_multigpu.py
+++ cleanrl/pqn_atari_envpool_multigpu.py
@@ -3,8 +3,11 @@ import os
 import random
 import time
 from collections import deque
-from dataclasses import dataclass
-
+import warnings
+from dataclasses import dataclass, field
+from typing import List, Literal
+from rich.pretty import pprint
+import torch.distributed as dist
 import envpool
 import gym
 import numpy as np
@@ -42,7 +45,7 @@ class Args:
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
-    num_envs: int = 256
+    local_num_envs: int = 256
     """the number of parallel game environments"""
     num_steps: int = 32
     """the number of steps to run in each environment per policy rollout"""
@@ -64,14 +67,26 @@ class Args:
     """the fraction of `total_timesteps` it takes from start_e to end_e"""
     q_lambda: float = 0.65
     """the lambda for the Q-Learning algorithm"""
+    device_ids: List[int] = field(default_factory=lambda: [])
+    """the device ids that subprocess workers will use"""
+    backend: Literal["gloo", "nccl", "mpi"] = "gloo"
+    """the backend for distributed training"""
 
     # to be filled in runtime
+    local_batch_size: int = 0
+    """the local batch size in the local rank (computed in runtime)"""
+    local_minibatch_size: int = 0
+    """the local mini-batch size in the local rank (computed in runtime)"""
+    num_envs: int = 0
+    """the number of parallel game environments (computed in runtime)"""
     batch_size: int = 0
     """the batch size (computed in runtime)"""
     minibatch_size: int = 0
     """the mini-batch size (computed in runtime)"""
     num_iterations: int = 0
     """the number of iterations (computed in runtime)"""
+    world_size: int = 0
+    """the number of processes (computed in runtime)"""
 
 
 class RecordEpisodeStatistics(gym.Wrapper):
@@ -138,36 +153,66 @@ def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
 
 
 if __name__ == "__main__":
+    # torchrun --standalone --nnodes=1 --nproc_per_node=2 ppo_atari_multigpu.py
+    # taken from https://pytorch.org/docs/stable/elastic/run.html
     args = tyro.cli(Args)
+    local_rank = int(os.getenv("LOCAL_RANK", "0"))
+    args.world_size = int(os.getenv("WORLD_SIZE", "1"))
+    args.local_batch_size = int(args.local_num_envs * args.num_steps)
+    args.local_minibatch_size = int(args.local_batch_size // args.num_minibatches)
+    args.num_envs = args.local_num_envs * args.world_size
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
     args.num_iterations = args.total_timesteps // args.batch_size
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
-    if args.track:
-        import wandb
-
-        wandb.init(
-            project=args.wandb_project_name,
-            entity=args.wandb_entity,
-            sync_tensorboard=True,
-            config=vars(args),
-            name=run_name,
-            monitor_gym=True,
-            save_code=True,
+    if args.world_size > 1:
+        dist.init_process_group(args.backend, rank=local_rank, world_size=args.world_size)
+    else:
+        warnings.warn(
+            """
+Not using distributed mode!
+If you want to use distributed mode, please execute this script with 'torchrun'.
+E.g., `torchrun --standalone --nnodes=1 --nproc_per_node=2 ppo_atari_multigpu.py`
+        """
         )
-    writer = SummaryWriter(f"runs/{run_name}")
-    writer.add_text(
-        "hyperparameters",
-        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
-    )
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    writer = None
+    if local_rank == 0:
+        if args.track:
+            import wandb
+
+            wandb.init(
+                project=args.wandb_project_name,
+                entity=args.wandb_entity,
+                sync_tensorboard=True,
+                config=vars(args),
+                name=run_name,
+                monitor_gym=True,
+                save_code=True,
+            )
+        writer = SummaryWriter(f"runs/{run_name}")
+        writer.add_text(
+            "hyperparameters",
+            "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+        )
+        pprint(args)
 
     # TRY NOT TO MODIFY: seeding
+    # CRUCIAL: note that we needed to pass a different seed for each data parallelism worker
+    args.seed += local_rank
     random.seed(args.seed)
     np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed - local_rank)
     torch.backends.cudnn.deterministic = args.torch_deterministic
 
-    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+    if len(args.device_ids) > 0:
+        assert len(args.device_ids) == args.world_size, "you must specify the same number of device ids as `--nproc_per_node`"
+        device = torch.device(f"cuda:{args.device_ids[local_rank]}" if torch.cuda.is_available() and args.cuda else "cpu")
+    else:
+        device_count = torch.cuda.device_count()
+        if device_count < args.world_size:
+            device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+        else:
+            device = torch.device(f"cuda:{local_rank}" if torch.cuda.is_available() and args.cuda else "cpu")
 
     # env setup
     envs = envpool.make(
@@ -230,6 +275,9 @@ if __name__ == "__main__":
             rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done = torch.Tensor(next_obs).to(device), torch.Tensor(next_done).to(device)
 
+            if not writer:
+                continue
+
             for idx, d in enumerate(next_done):
                 if d and info["lives"][idx] == 0:
                     print(f"global_step={global_step}, episodic_return={info['r'][idx]}")
@@ -238,6 +286,9 @@ if __name__ == "__main__":
                     writer.add_scalar("charts/episodic_return", info["r"][idx], global_step)
                     writer.add_scalar("charts/episodic_length", info["l"][idx], global_step)
 
+        print(
+            f"local_rank: {local_rank}, action.sum(): {action.sum()}, iteration: {iteration}"
+        )
         # Compute Q(lambda) targets
         with torch.no_grad():
             returns = torch.zeros_like(rewards).to(device)
@@ -259,11 +310,11 @@ if __name__ == "__main__":
         b_returns = returns.reshape(-1)
 
         # Optimizing the Q-network
-        b_inds = np.arange(args.batch_size)
+        b_inds = np.arange(args.local_batch_size)
         for epoch in range(args.update_epochs):
             np.random.shuffle(b_inds)
-            for start in range(0, args.batch_size, args.minibatch_size):
-                end = start + args.minibatch_size
+            for start in range(0, args.local_batch_size, args.local_minibatch_size):
+                end = start + args.local_minibatch_size
                 mb_inds = b_inds[start:end]
 
                 old_val = q_network(b_obs[mb_inds]).gather(1, b_actions[mb_inds].unsqueeze(-1).long()).squeeze()
@@ -272,13 +323,31 @@ if __name__ == "__main__":
                 # optimize the model
                 optimizer.zero_grad()
                 loss.backward()
+                
+                if args.world_size > 1:
+                    # batch allreduce ops: see https://github.com/entity-neural-network/incubator/pull/220
+                    all_grads_list = []
+                    for param in q_network.parameters():
+                        if param.grad is not None:
+                            all_grads_list.append(param.grad.view(-1))
+                    all_grads = torch.cat(all_grads_list)
+                    dist.all_reduce(all_grads, op=dist.ReduceOp.SUM)
+                    offset = 0
+                    for param in q_network.parameters():
+                        if param.grad is not None:
+                            param.grad.data.copy_(
+                                all_grads[offset : offset + param.numel()].view_as(param.grad.data) / args.world_size
+                            )
+                            offset += param.numel()
+                            
                 nn.utils.clip_grad_norm_(q_network.parameters(), args.max_grad_norm)
                 optimizer.step()
 
-        writer.add_scalar("losses/td_loss", loss, global_step)
-        writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
-        print("SPS:", int(global_step / (time.time() - start_time)))
-        writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
+        if local_rank == 0:
+            writer.add_scalar("losses/td_loss", loss, global_step)
+            writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
+            print("SPS:", int(global_step / (time.time() - start_time)))
+            writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
 
     envs.close()
     writer.close()
